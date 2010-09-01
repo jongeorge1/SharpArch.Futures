ebaedc1fde97b1dabcc51b0edab72fec92aaccd9
Jonathan George <jon@jonathangeorge.co.uk>
2010-09-01 17:49:20 +0100

    Build stuff

---
+++ Solutions/CommonAssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SharpArch Development Team")]
+[assembly: AssemblyProduct("SharpArch.Futures")]
+[assembly: AssemblyCopyright("Copyright © Sharp Architecture Development Team 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
